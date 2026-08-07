0463eae3ffcaebb7a7576a46d6b1d88e36fa880d
lute0x0 <83352167+lute0x0@users.noreply.github.com>
2026-08-08 06:15:55 +0900

    フォント設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "JK Gothic",
+      cssVariable: "--font-jk-gothic",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/JKG-L_3.woff2"],
+            weight: "normal",
+            style: "normal",
+          },
+        ],
+      },
+    },
+  ],
+});
